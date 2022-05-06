d75f9cee8bf30798de069fa3041f17a0c01a63ac
fatihsen20 <fatih.sn2000@gmail.com>
2022-05-06 11:15:46 +0300

    -Yeni model oluşturuldu.Artık daha iyi sonuçlar dönüyor. -Fire detector alarmı eklendi.

--- webcam.py
+++ webcam.py
@@ -3,19 +3,23 @@ import numpy as np
 from PIL import Image
 import tensorflow as tf
 from keras.preprocessing import image
+from playsound import playsound
 
+def play_alarm_sound_function():
+	while True:
+		playsound.playsound('alarm-sound.mp3',True)
 #Load the saved model
-model = tf.keras.models.load_model('super_final.h5')
+model = tf.keras.models.load_model('super_final2.h5')
 video = cv2.VideoCapture(0)
 Catagories=['NonFire','Fire']
+alarmStatus = False
 while True:
   _, frame = video.read()
   #Convert the captured frame into RGB
   im = Image.fromarray(frame, 'RGB')
-  #Resizing into 224x224 because we trained the model with this image size.
+  #Resizing into 300x300 because we trained the model with this image size.
   im = im.resize((300,300))
   img_array = image.img_to_array(im)
-  # img_array = np.expand_dims(img_array, axis=0) / 255
   img_array /= 255.0
   img_array = img_array.reshape(300,300,3)
   img_array = np.expand_dims(img_array,axis = 0) 
@@ -25,29 +29,10 @@ while True:
   if Catagories[int(prediction)]== "Fire":
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     print("fire!!!")
-  #if prediction is 0, which means there is fire in the frame.
-  # if prediction <= 0.5:
-  #   frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-  #   print(prediction)
+    playsound('alarm.mp3')
   cv2.imshow("Capturing", frame)
   key=cv2.waitKey(1)
   if key == ord('q'):
     break
 video.release()
 cv2.destroyAllWindows()
-
-# test_image = image.load_img("C:/Users/Numan/Desktop/fire/indir.jpg",target_size=(300,300))
-# test_image = image.img_to_array(test_image)
-# test_image = test_image/255
-# test_image = np.expand_dims(test_image,axis=0)
-
-# #Predicting the class of the image
-# result = model.predict(test_image)
-
-# Catagories=['Fire','Smoke']
-
-# image_show = PIL.Image.open("C:/Users/Numan/Desktop/fire/indir.jpg")
-
-
-# plt.title(Catagories[int(result[0][0])])
-# plt.imshow(image_show)
